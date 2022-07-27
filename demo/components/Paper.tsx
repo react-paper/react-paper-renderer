d45c9260577c565e776aaada7e21fcb745bc15c4
Bojan Hribernik <bojan.hribernik@gmail.com>
2022-07-28 00:32:07 +0200

    Editor demo, work in progress

--- demo/components/Paper.tsx
+++ demo/components/Paper.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useCallback, useContext } from "react";
+import { Canvas, View, Layer, Raster } from "react-paper-renderer";
 import useMeasure from "react-use-measure";
-import { Canvas, CanvasProps, View, Layer, Raster } from "react-paper-renderer";
 
 import { Context } from "./context";
 import { Items } from "./items";
@@ -9,17 +9,16 @@ import { Image, Route } from "./types";
 import { fitImage } from "./utils/fitImage";
 import { initScope } from "./utils/initScope";
 
-const containerStyles = {
-  width: "100%",
-  height: "100%",
-};
+import { Tool, ToolName } from "./tools";
 
-type Props = Omit<CanvasProps, "width" | "height"> & {
+import styles from "./Paper.module.css";
+
+type Props = {
   image: Image;
 };
 
-export const Paper: FC<Props> = ({ children, image, ...other }) => {
-  const [ref, { width, height }] = useMeasure();
+export const Paper: FC<Props> = ({ image }) => {
+  const [ref, { width, height }] = useMeasure({ debounce: 150 });
   const value = useContext(Context);
   const [state, dispatch] = value;
 
@@ -44,46 +43,62 @@ export const Paper: FC<Props> = ({ children, image, ...other }) => {
     [image, dispatch]
   );
 
+  const setTool = (tool: ToolName) => dispatch({ type: "setTool", tool });
+
   return (
-    <div style={containerStyles} ref={ref}>
-      {width > 0 && height > 0 && (
-        <Canvas
-          {...other}
-          width={width}
-          height={height}
-          onScopeReady={handleScopeReady}
-        >
-          <Context.Provider value={value}>
-            <View>
-              <Layer id={image.id} visible={!!state.image}>
-                <Raster locked source={image.url} onLoad={handleImageLoad} />
-              </Layer>
-              {historyImage &&
-                historyImage.routes.map((route, index) => (
-                  <Layer
-                    id={route.id}
-                    key={route.id}
-                    active={state.routeIndex === index}
-                    visible={!!state.image}
-                  >
-                    {route.items.map((item) => {
-                      const Item = Items[item.type];
-                      return (
-                        <Item
-                          {...item}
-                          id={item.id}
-                          key={item.id}
-                          selected={state.selection === item.id}
-                        />
-                      );
-                    })}
-                  </Layer>
-                ))}
-              {children}
-            </View>
-          </Context.Provider>
-        </Canvas>
-      )}
+    <div className={styles.container}>
+      <div className={styles.toolbar}>
+        <button onClick={() => setTool(ToolName.Move)}>{ToolName.Move}</button>
+        <button onClick={() => setTool(ToolName.Pen)}>{ToolName.Pen}</button>
+        <button onClick={() => setTool(ToolName.Circle)}>{ToolName.Circle}</button>
+        <button onClick={() => setTool(ToolName.Select)}>{ToolName.Select}</button>
+        <button onClick={() => setTool(ToolName.Delete)}>{ToolName.Delete}</button>
+        <button onClick={() => dispatch({ type: "undo" })}>Undo</button>
+        <button onClick={() => dispatch({ type: "redo" })}>Redo</button>
+        <button onClick={() => console.log(JSON.stringify(state.scope?.exportJSON()))}>Save</button>
+      </div>
+      <div className={styles.paper} ref={ref}>
+        {width > 0 && height > 0 && (
+          <Canvas
+            className={styles.canvas}
+            width={width}
+            height={height}
+            onScopeReady={handleScopeReady}
+          >
+            <Context.Provider value={value}>
+              <View>
+                <Layer id={image.id} visible={!!state.image}>
+                  <Raster locked source={image.url} onLoad={handleImageLoad} />
+                </Layer>
+                {historyImage &&
+                  historyImage.routes.map((route, index) => (
+                    <Layer
+                      id={route.id}
+                      key={route.id}
+                      name={route.name}
+                      active={state.routeIndex === index}
+                      visible={!!state.image}
+                    >
+                      {route.items.map((item) => {
+                        const Item = Items[item.type];
+                        return (
+                          <Item
+                            {...item}
+                            id={item.id}
+                            key={item.id}
+                            selected={state.selection === item.id}
+                          />
+                        );
+                      })}
+                    </Layer>
+                  ))}
+              </View>
+              <Tool.Move />
+              <Tool.Pen />
+            </Context.Provider>
+          </Canvas>
+        )}
+      </div>
     </div>
   );
 };
